45dd10f8bf9f06f58ee0b7445db4a2a1e776018b
Khalil Alford <khalilalford@Khalils-MacBook-Pro.local>
2023-06-30 22:20:00 -0700

    created variable that is referencing the button from the html

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var confirmUppercase;
 var confirmLowercase;
 var confirmNumbers;
 var confirmSpecialChar;
-// added arrays to hold my numbers, letters, and characters
+
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var numbers = [0,1,2,3,4,5,6,7,8,9]
